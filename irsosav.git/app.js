5d7b65872d9ddea2fe164a36f23281496f618520
Christian Cozot <irsosav@ip-172-31-57-8.ec2.internal>
2016-05-14 16:37:15 -0300

    Se mejora la forma de obtener los datos del metodo GET para el recurso /login

--- irsosav.git/app.js
+++ irsosav.git/app.js
@@ -21,9 +21,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Content-Type: application/json");
+  next();
+});
 
-app.use('/', routes);
-app.use('/users', users);
+
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- irsosav.git/routes/index.js
+++ irsosav.git/routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
-var router = express.Router();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var md5 = require('md5');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var app = express();
+
+var dbconn = mysql.createConnection({
+  host: '127.0.0.1',
+  database: 'IRSOSAV',
+  user: 'irsosav',
+  password: 'motor970',
+  port: '3306',
+  //debug: true
 });
 
-module.exports = router;
+dbconn.connect(function(err){
+  if(err){
+    console.log('Error conectando con la DB');
+    return;
+  }
+  console.log('Conexion establecida');
+});
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/login', function(request, response) {
+  dbconn.query('SELECT * FROM cliente', function(err, rows) {
+    if(err) throw err;    
+    //for (var i = 0; i < rows.length; i++) {
+    //  console.log(rows[i].name);
+    //  response.jsonp(rows[i].id_ciudad); //Envia solo el campo documento
+    //  response.jsonp(rows);
+    var empleados=[];
+    for (var i = 0; i < rows.length; i++) {
+      var empleado={};
+      empleado.descripcion=rows[i].descripcion;
+      empleados.push(empleado);
+    };
+       response.jsonp(empleados);
+  });
+});
+
+app.get('/login/:uid', function(request, response) {
+  dbconn.query("SELECT * FROM cliente where documento=" +request.params.uid, function(err, rows) {
+    if(err) throw err;    
+//    for (var i = 0; i < rows.length; i++) {
+    //console.log(rows);
+//      response.jsonp(rows[i]);
+//     };
+//  });
+//    var empleados=[];
+//    for (var i = 0; i < rows.length; i++) {
+//      var empleado={};
+//      empleado.descripcion=rows[i].descripcion;
+//      empleados.push(empledo);
+//    }
+       response.jsonp(rows);
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
