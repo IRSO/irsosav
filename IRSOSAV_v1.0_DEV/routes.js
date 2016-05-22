03a82a5f8961fb687dee9c8ad787c8370e67b0aa
Christian Cozot <irsosav@ip-172-31-57-8.ec2.internal>
2016-05-22 16:23:03 -0300

    Se agrega entorno de desarollo IRSOSAV_1.0_DEV

---
+++ IRSOSAV_v1.0_DEV/routes.js
@@ -0,0 +1,30 @@
+var todo = require('./models/todo');
+
+module.exports = {
+  configure: function(app) {
+    app.get('/todo/', function(req, res) {
+      todo.get(res);
+    });
+
+    app.post('/todo/', function(req, res) {
+      todo.create(req.body, res);
+    });
+
+    app.put('/todo/', function(req, res) {
+      todo.update(req.body, res);
+    });
+
+    app.delete('/todo/:id/', function(req, res) {
+      todo.delete(req.params.id, res);
+    });
+
+    app.get('/test/:uid', function(req, res) {
+    var test=[];
+    var texto={};
+    texto.id = ""+req.params.uid;
+    texto.content = "Hola mundo";
+    test.push(texto);
+    res.jsonp(test);
+  });
+  }
+};
