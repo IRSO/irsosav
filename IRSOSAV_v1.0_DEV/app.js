e805524752e6ba4d383411ab6e262b91d2cd18de
Christian Cozot <irsosav@ip-172-31-57-8.ec2.internal>
2016-05-26 21:25:57 -0300

    Se agregaron los headers

--- IRSOSAV_v1.0_DEV/app.js
+++ IRSOSAV_v1.0_DEV/app.js
@@ -5,6 +5,17 @@ var routes = require('./routes');
 //var md5 = require('MD5');
 
 var app = express();
+
+app.use(function(err,req,res,next) {
+  if(err){
+    res.jsonp(err);
+  } 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  next();
+});
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
